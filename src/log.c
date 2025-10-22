750d6d0b895e0a08604bccf206274fdcccd3fe98
Chris Novakovic <chris@chrisn.me.uk>
2025-10-22 16:20:39 +0100

    Disable reporting of log source at compile time (#1)
    By default, disable the reporting of the file name and line number on
    which `log_log` (and its convenience macros) are called. Only output
    this information if the `LOG_SOURCE` macro is defined.

--- src/log.c
+++ src/log.c
@@ -54,15 +54,32 @@ static void stdout_callback(log_Event *ev) {
   char buf[16];
   buf[strftime(buf, sizeof(buf), "%H:%M:%S", ev->time)] = '\0';
 #ifdef LOG_USE_COLOR
+
+#ifdef LOG_SOURCE
   fprintf(
     ev->udata, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
     buf, level_colors[ev->level], level_strings[ev->level],
     ev->file, ev->line);
 #else
   fprintf(
+    ev->udata, "%s %s%-5s\x1b[0m: ",
+    buf, level_colors[ev->level], level_strings[ev->level]);
+#endif /* LOG_SOURCE */
+
+#else /* LOG_USE_COLOR */
+
+#ifdef LOG_SOURCE
+  fprintf(
     ev->udata, "%s %-5s %s:%d: ",
     buf, level_strings[ev->level], ev->file, ev->line);
-#endif
+#else
+  fprintf(
+    ev->udata, "%s %-5s: ",
+    buf, level_strings[ev->level]);
+#endif /* LOG_SOURCE */
+
+#endif /* LOG_USE_COLOR */
+
   vfprintf(ev->udata, ev->fmt, ev->ap);
   fprintf(ev->udata, "\n");
   fflush(ev->udata);
@@ -72,9 +89,17 @@ static void stdout_callback(log_Event *ev) {
 static void file_callback(log_Event *ev) {
   char buf[64];
   buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", ev->time)] = '\0';
+
+#ifdef LOG_SOURCE
   fprintf(
     ev->udata, "%s %-5s %s:%d: ",
     buf, level_strings[ev->level], ev->file, ev->line);
+#else
+  fprintf(
+    ev->udata, "%s %-5s: ",
+    buf, level_strings[ev->level]);
+#endif
+
   vfprintf(ev->udata, ev->fmt, ev->ap);
   fprintf(ev->udata, "\n");
   fflush(ev->udata);
@@ -137,6 +162,28 @@ static void init_event(log_Event *ev, void *udata) {
 }
 
 
+static void log_event(log_Event *ev) {
+  lock();
+
+  if (!L.quiet && ev->level >= L.level) {
+    init_event(ev, stderr);
+    stdout_callback(ev);
+  }
+
+  for (int i = 0; i < MAX_CALLBACKS && L.callbacks[i].fn; i++) {
+    Callback *cb = &L.callbacks[i];
+    if (ev->level >= cb->level) {
+      init_event(ev, cb->udata);
+      cb->fn(ev);
+    }
+  }
+
+  unlock();
+}
+
+
+#ifdef LOG_SOURCE
+
 void log_log(int level, const char *file, int line, const char *fmt, ...) {
   log_Event ev = {
     .fmt   = fmt,
@@ -144,25 +191,21 @@ void log_log(int level, const char *file, int line, const char *fmt, ...) {
     .line  = line,
     .level = level,
   };
-
-  lock();
-
-  if (!L.quiet && level >= L.level) {
-    init_event(&ev, stderr);
-    va_start(ev.ap, fmt);
-    stdout_callback(&ev);
-    va_end(ev.ap);
-  }
-
-  for (int i = 0; i < MAX_CALLBACKS && L.callbacks[i].fn; i++) {
-    Callback *cb = &L.callbacks[i];
-    if (level >= cb->level) {
-      init_event(&ev, cb->udata);
-      va_start(ev.ap, fmt);
-      cb->fn(&ev);
-      va_end(ev.ap);
-    }
-  }
-
-  unlock();
+  va_start(ev.ap, fmt);
+  log_event(&ev);
+  va_end(ev.ap);
 }
+
+#else
+
+void log_log(int level, const char *fmt, ...) {
+  log_Event ev = {
+    .fmt   = fmt,
+    .level = level,
+  };
+  va_start(ev.ap, fmt);
+  log_event(&ev);
+  va_end(ev.ap);
+}
+
+#endif /* LOG_SOURCE */
